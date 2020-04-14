36635e41df563f43257688a895ebddf4e1549cd4
Harri Feng <harrifeng@gmail.com>
2020-04-14 16:41:19 +0800

    new ok

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-sudo cp -f gocron-node /usr/local/bin/gocron-node
-sudo cp -f gocron-node.service /usr/lib/systemd/system/gocron-node.service
-
-sudo systemctl enable gocron-node
-sudo systemctl start gocron-node
-sudo systemctl status gocron-node
+nohup ./gocron-node &
