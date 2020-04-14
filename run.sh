98f282d530ba1fae398b7cfb4a91264f84b08fdd
Harri Feng <harrifeng@gmail.com>
2020-04-14 16:28:39 +0800

    new ok

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
-mkdir -p ${HOME}/.config/systemd/user
-mkdir -p ${HOME}/.bin
-cp -f gocron-node ${HOME}/.bin/gocron-node
+sudo cp -f gocron-node /usr/local/bin/gocron-node
+sudo cp -f gocron-node.service /usr/lib/systemd/system/gocron-node.service
 
-cp -f gocron-node.service ${HOME}/.config/systemd/user/gocron-node.service
-sed -i "s|REPLACE_USERNAME|$HOME|g" ${HOME}/.config/systemd/user/gocron-node.service
-
-systemctl enable --user gocron-node
-systemctl start --user gocron-node
-systemctl status --user gocron-node
+sudo systemctl enable gocron-node
+sudo systemctl start gocron-node
+sudo systemctl status gocron-node
